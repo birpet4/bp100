19f4ad71cb087bae659cfea9cdc1bd2be2567c67
Peter Bircher <peter_bircher@revenue.ai>
2023-05-10 13:15:39 +0200

    add home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import s from "./Home.module.scss";
+
 const Home = () => {
-    return <>Home page of BP100</>
+    return <div className={s.container}>
+        <Link to={"questions"} className={s.link}>Questions game</Link>
+        <Link to={"trueorfalse"} className={s.link}>True or false game</Link>
+        <Link to={"pair"} className={s.link}>Pairing game</Link>
+        <Link to={"pictures"} className={s.link}>Picture guessing game</Link>
+        <Link to={"picturespair"} className={s.link}>Pictures pairing game</Link>
+    </div>
 }
 
 export default Home;
